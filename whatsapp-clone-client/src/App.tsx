5c9a419a489b227348a9b9df3ae8905b952e8c97
Guilherme Porto <guiportooo@gmail.com>
2020-05-18 19:49:31 -0300

    Add ChatRoomScreen and implement navigation to it

--- whatsapp-clone-client/src/App.tsx
+++ whatsapp-clone-client/src/App.tsx
@@ -1,10 +1,30 @@
 import React from 'react';
+import {
+  BrowserRouter,
+  Route,
+  Redirect,
+  RouteComponentProps,
+} from 'react-router-dom';
+import GlideSwitch from './components/GlideSwitch';
 import ChatsListScreen from './components/ChatsListScreen';
+import ChatRoomScreen from './components/ChatRoomScreen';
 
 const App: React.FC = () => (
-  <div>
-    <ChatsListScreen />
-  </div>
+  <BrowserRouter>
+    <GlideSwitch>
+      <Route exact path="/chats" component={ChatsListScreen} />
+      <Route
+        exact
+        path="/chats/:chatId"
+        component={({ match }: RouteComponentProps<{ chatId: string }>) => (
+          <ChatRoomScreen chatId={match.params.chatId}></ChatRoomScreen>
+        )}
+      />
+    </GlideSwitch>
+    <Route exact path="/" render={redirectToChats} />
+  </BrowserRouter>
 );
+
+const redirectToChats = () => <Redirect to="/chats" />;
 
 export default App;
